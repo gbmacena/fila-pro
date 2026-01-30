29c5f74fbffd577126ef80b42d86df9b54eed873
Gabriel Macena <gabriel.macena10@hotmail.com>
2026-01-30 11:58:51 -0300

    fix: ajuste rps

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,7 +14,7 @@ import { CommonModule } from './common/common.module';
     ThrottlerModule.forRoot([
       {
         ttl: 60000,
-        limit: 10,
+        limit: 100,
       },
     ]),
     CommonModule,
